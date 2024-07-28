4bb4ee984a852c77a863aac639fdff605808ba7e
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 15:19:47 +0200

    Player Repository Methods Implementation.

--- MiniLotto.API/Models/Repository/IPlayerRepository.cs
+++ MiniLotto.API/Models/Repository/IPlayerRepository.cs
@@ -6,6 +6,6 @@ namespace MiniLotto.API.Models.Repository;
 public interface IPlayerRepository
 {
     Task<Result<Player>> AddPlayerAsync(Player player);
-    Task<Player> GetPlayerAsync(int id);
-    Task<IEnumerable<Player>> GetAllPlayersAsync();
+    Task<Result<Player>> GetPlayerAsync(int id);
+    Task<Result<IEnumerable<Player>>> GetAllPlayersAsync();
 }

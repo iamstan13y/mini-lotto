bd68a2b6b91212a4c8aa1809cf4beace5ed76503
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 22:44:17 +0200

    Player Number Set Fixes & DB Migrations & Stuff!

--- MiniLotto.API/Models/Data/Player.cs
+++ MiniLotto.API/Models/Data/Player.cs
@@ -4,5 +4,5 @@ public class Player
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<int> Numbers { get; set; } = [];
+    public List<NumberSet> NumberSets { get; set; } = [];
 }

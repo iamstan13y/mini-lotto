bf18ea8ed8de41ec18fb0d26a0b44e696c503f68
Keith Stanley <iamstan13y@gmail.com>
2024-07-29 00:44:49 +0200

    A Bunch of Code that Works.

---
+++ MiniLotto.Web/Services/HttpService.cs
@@ -0,0 +1,26 @@
+﻿using MiniLotto.Web.Extensions;
+using MiniLotto.Web.Models.Data;
+using MiniLotto.Web.Models.Local;
+
+namespace MiniLotto.Web.Services;
+
+public class HttpService(HttpClient client) : IHttpService
+{
+    private readonly HttpClient _client = client;
+
+    public async Task<Result<Player>> AddPlayerAsync(PlayerRequest request)
+    {
+        var response = await _client.PostAsJson($"/api/v1/Player/add", request);
+        return await response.ReadContentAs<Result<Player>>();
+    }
+
+    public Task<Result<IEnumerable<Player>>> GetAllPlayersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<Player>> GetPlayerAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+}

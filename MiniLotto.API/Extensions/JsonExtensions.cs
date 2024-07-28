8addf5e7582f855899311fbb29c91988bbd3e330
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 22:54:54 +0200

    Remove Cycles Json Extension Method.

---
+++ MiniLotto.API/Extensions/JsonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace MiniLotto.API.Extensions;
+
+public static class JsonExtensions
+{
+    public static T? RemoveCycles<T>(this T obj)
+    {
+        var options = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+
+        };
+
+        var serializedObj = JsonSerializer.Serialize(obj, options);
+
+        return JsonSerializer.Deserialize<T?>(serializedObj, options);
+    }
+}

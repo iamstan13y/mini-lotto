a2d86d4700baa3e80c3d5b2e92fe67d59e2a68b0
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 15:19:49 +0200

    Player Repository Methods Implementation.

--- MiniLotto.API/Models/Repository/PlayerRepository.cs
+++ MiniLotto.API/Models/Repository/PlayerRepository.cs
@@ -1,22 +1,49 @@
-﻿using MiniLotto.API.Models.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MiniLotto.API.Models.Data;
 using MiniLotto.API.Models.Local;
 
 namespace MiniLotto.API.Models.Repository;
 
 public class PlayerRepository : IPlayerRepository
 {
-    public Task<Result<Player>> AddPlayerAsync(Player player)
+    private readonly AppDbContext _context;
+
+    public PlayerRepository(AppDbContext context) => _context = context;
+    
+    public async Task<Result<Player>> AddPlayerAsync(Player player)
     {
-        throw new NotImplementedException();
+        try
+        {
+            await _context.Players.AddAsync(player);
+            await _context.SaveChangesAsync();
+            
+            return new Result<Player>(player);
+        }
+        catch (Exception ex)
+        {
+            return new Result<Player>(false, ex.Message);
+        }
     }
 
-    public Task<IEnumerable<Player>> GetAllPlayersAsync()
+    public async Task<Result<IEnumerable<Player>>> GetAllPlayersAsync()
     {
-        throw new NotImplementedException();
+        return new Result<IEnumerable<Player>>(await _context.Players.ToListAsync());
     }
 
-    public Task<Player> GetPlayerAsync(int id)
+    public async Task<Result<Player>> GetPlayerAsync(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var player = await _context.Players.FindAsync(id);
+
+            if (player == null) return new Result<Player>(false, "Player not found");
+            
+            return new Result<Player>(player);
+        }
+        catch (Exception ex)
+        {
+            return new Result<Player>(false, ex.Message);
+        }
+       
     }
 }

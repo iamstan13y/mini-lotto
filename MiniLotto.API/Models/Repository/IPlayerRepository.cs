b09a54326bb957f52c5da410d67a41f7e41f74a4
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 16:16:48 +0200

    Player Controller & Stuff.

--- MiniLotto.API/Models/Repository/IPlayerRepository.cs
+++ MiniLotto.API/Models/Repository/IPlayerRepository.cs
@@ -6,6 +6,6 @@ namespace MiniLotto.API.Models.Repository;
 public interface IPlayerRepository
 {
     Task<Result<Player>> AddPlayerAsync(Player player);
-    Task<Result<Player>> GetPlayerAsync(int id);
+    Task<Result<Player>> GetPlayerAsync(string name);
     Task<Result<IEnumerable<Player>>> GetAllPlayersAsync();
 }

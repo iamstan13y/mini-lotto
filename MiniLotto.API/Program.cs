a71f2b25c758d0a04fca462baebb2b210913ffd2
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 15:03:02 +0200

    Player Repository Pattern Config.

--- MiniLotto.API/Program.cs
+++ MiniLotto.API/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using MiniLotto.API.Models;
+using MiniLotto.API.Models.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

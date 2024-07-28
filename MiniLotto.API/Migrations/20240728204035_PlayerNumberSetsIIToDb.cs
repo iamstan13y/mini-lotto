845c7d44c57b8947b41916c92a678ed16cd04c80
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 22:44:04 +0200

    Player Number Set Fixes & DB Migrations & Stuff!

---
+++ MiniLotto.API/Migrations/20240728204035_PlayerNumberSetsIIToDb.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniLotto.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerNumberSetsIIToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NumberSet_Players_PlayerId",
+                table: "NumberSet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NumberSet",
+                table: "NumberSet");
+
+            migrationBuilder.RenameTable(
+                name: "NumberSet",
+                newName: "NumberSets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_NumberSet_PlayerId",
+                table: "NumberSets",
+                newName: "IX_NumberSets_PlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NumberSets",
+                table: "NumberSets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NumberSets_Players_PlayerId",
+                table: "NumberSets",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NumberSets_Players_PlayerId",
+                table: "NumberSets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NumberSets",
+                table: "NumberSets");
+
+            migrationBuilder.RenameTable(
+                name: "NumberSets",
+                newName: "NumberSet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_NumberSets_PlayerId",
+                table: "NumberSet",
+                newName: "IX_NumberSet_PlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NumberSet",
+                table: "NumberSet",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NumberSet_Players_PlayerId",
+                table: "NumberSet",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

01b1c07f91e4591646c387177b739936991589c7
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 15:02:57 +0200

    Player Repository Pattern Config.

---
+++ MiniLotto.API/Models/Repository/IPlayerRepository.cs
@@ -0,0 +1,11 @@
+﻿using MiniLotto.API.Models.Data;
+using MiniLotto.API.Models.Local;
+
+namespace MiniLotto.API.Models.Repository;
+
+public interface IPlayerRepository
+{
+    Task<Result<Player>> AddPlayerAsync(Player player);
+    Task<Player> GetPlayerAsync(int id);
+    Task<IEnumerable<Player>> GetAllPlayersAsync();
+}

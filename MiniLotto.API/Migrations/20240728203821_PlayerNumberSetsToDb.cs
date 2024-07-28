76b2a0e7891e259328b7749d1ff6a8bcc0cb72f5
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 22:43:57 +0200

    Player Number Set Fixes & DB Migrations & Stuff!

---
+++ MiniLotto.API/Migrations/20240728203821_PlayerNumberSetsToDb.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniLotto.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerNumberSetsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Numbers",
+                table: "Players");
+
+            migrationBuilder.CreateTable(
+                name: "NumberSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numbers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NumberSet", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NumberSet_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NumberSet_PlayerId",
+                table: "NumberSet",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NumberSet");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Numbers",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

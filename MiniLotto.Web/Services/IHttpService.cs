bc57dc5cc385c3725e27017c94ba4d10d541645f
Keith Stanley <iamstan13y@gmail.com>
2024-07-29 00:44:52 +0200

    A Bunch of Code that Works.

---
+++ MiniLotto.Web/Services/IHttpService.cs
@@ -0,0 +1,11 @@
+﻿using MiniLotto.Web.Models.Data;
+using MiniLotto.Web.Models.Local;
+
+namespace MiniLotto.Web.Services;
+
+public interface IHttpService
+{
+    Task<Result<Player>> AddPlayerAsync(PlayerRequest request);
+    Task<Result<Player>> GetPlayerAsync(string name);
+    Task<Result<IEnumerable<Player>>> GetAllPlayersAsync();
+}

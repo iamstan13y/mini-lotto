8e00e095683478663808f6d4c7f27cf9aa2b59dc
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 15:42:40 +0200

    Player Service Config.

---
+++ MiniLotto.API/Services/PlayerService.cs
@@ -0,0 +1,47 @@
+﻿using MiniLotto.API.Models.Local;
+using MiniLotto.API.Models.Repository;
+
+namespace MiniLotto.API.Services;
+
+public class PlayerService : IPlayerService
+{
+    private readonly IPlayerRepository _playerRepository;
+    private static readonly Random _random = new();
+
+    public PlayerService(PlayerRepository playerRepository)
+    {
+        _playerRepository = playerRepository;
+    }
+
+    public async Task<Result<DrawResult>> RunDrawAsync()
+    {
+        var users = (await _playerRepository.GetAllPlayersAsync()).Data;
+        var winningNumbers = GenerateWinningNumbers();
+        
+        var winners = users?
+            .Where(u => u.Numbers.Intersect(winningNumbers)
+            .Count() >= 3)
+            .Select(u => u.Name)
+            .ToList();
+
+        return new Result<DrawResult>(new DrawResult
+        {
+            WinningNumbers = winningNumbers,
+            Winners = winners
+        });
+    }
+
+    private List<int> GenerateWinningNumbers()
+    {
+        var numbers = new List<int>();
+        while (numbers.Count < 6)
+        {
+            int number = _random.Next(1, 13);
+            if (!numbers.Contains(number))
+            {
+                numbers.Add(number);
+            }
+        }
+        return numbers;
+    }
+}

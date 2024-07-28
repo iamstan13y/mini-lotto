95083bf5905d901183bae58ce3f5ca1a59e1b6a8
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 16:17:00 +0200

    Player Controller & Stuff.

--- MiniLotto.API/Program.cs
+++ MiniLotto.API/Program.cs
@@ -8,10 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPlayerService, PlayerService>();
 
 builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 
-builder.Services.AddScoped<IPlayerService, PlayerService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

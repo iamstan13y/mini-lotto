f54502b579bc3d79986740723d5f1e7ad8c59c57
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 22:44:20 +0200

    Player Number Set Fixes & DB Migrations & Stuff!

--- MiniLotto.API/Models/Repository/PlayerRepository.cs
+++ MiniLotto.API/Models/Repository/PlayerRepository.cs
@@ -1,24 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MiniLotto.API.Models.Data;
 using MiniLotto.API.Models.Local;
 
 namespace MiniLotto.API.Models.Repository;
 
-public class PlayerRepository : IPlayerRepository
+public class PlayerRepository(AppDbContext context) : IPlayerRepository
 {
-    private readonly AppDbContext _context;
+    private readonly AppDbContext _context = context;
 
-    public PlayerRepository(AppDbContext context) => _context = context;
-    
     public async Task<Result<Player>> AddPlayerAsync(Player player)
     {
         try
         {
-            await _context.Players.AddAsync(player);
+            var existingPlayer = await _context.Players.Include(u => u.NumberSets).FirstOrDefaultAsync(u => u.Name == player.Name);
+
+            if (existingPlayer is not null)
+                existingPlayer.NumberSets.AddRange(player.NumberSets);
+            else
+                await _context.Players.AddAsync(player);
+
             await _context.SaveChangesAsync();
-            
             return new Result<Player>(player);
+
         }
         catch (Exception ex)
         {
@@ -28,17 +31,24 @@ public class PlayerRepository : IPlayerRepository
 
     public async Task<Result<IEnumerable<Player>>> GetAllPlayersAsync()
     {
-        return new Result<IEnumerable<Player>>(await _context.Players.ToListAsync());
+        var players = await _context.Players
+            .Include(x => x.NumberSets)
+            .ToListAsync();
+
+        return new Result<IEnumerable<Player>>(players);
     }
 
     public async Task<Result<Player>> GetPlayerAsync(string name)
     {
         try
         {
-            var player = await _context.Players.FirstOrDefaultAsync(x => x.Name == name);
+            var player = await _context
+                .Players
+                .Include(x => x.NumberSets)
+                .FirstOrDefaultAsync(x => x.Name == name);
 
             if (player == null) return new Result<Player>(false, "Player not found");
-            
+
             return new Result<Player>(player);
         }
         catch (Exception ex)

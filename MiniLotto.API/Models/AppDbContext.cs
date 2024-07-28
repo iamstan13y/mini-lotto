b2cc6e2dfb33c78568955be6a2eb08fb409f3d13
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 14:04:01 +0200

    NuGet Packages & DB Context Config.

---
+++ MiniLotto.API/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniLotto.API.Models.Data;
+
+namespace MiniLotto.API.Models;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Player> Players { get; set; }
+}

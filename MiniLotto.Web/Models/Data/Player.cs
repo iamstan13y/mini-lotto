b7310aca69585d9cc121061fb72f516cfa74a218
Keith Stanley <iamstan13y@gmail.com>
2024-07-29 00:44:35 +0200

    A Bunch of Code that Works.

---
+++ MiniLotto.Web/Models/Data/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniLotto.Web.Models.Data;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<NumberSet> NumberSets { get; set; } = [];
+}

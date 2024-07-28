9309a0fa6af2ffaa4be427b04d599b0e4857a05c
Keith Stanley <iamstan13y@gmail.com>
2024-07-28 14:30:05 +0200

    Player Model & DB Migrations.

---
+++ MiniLotto.API/Models/Data/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniLotto.API.Models.Data;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<int> Numbers { get; set; } = [];
+}
